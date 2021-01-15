2cf3f75e9b43fd8f34b06deb2670190a51549508
Logan Brock <36110596+lgbrock@users.noreply.github.com>
2021-01-15 12:17:45 -0600

    JS working

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 //Please create a function that takes in an array of numbers and returns a new array that only contains odd numbers.
 
 function newArrOdd(arr) {
-  //create an empty array to hold the old values
   let newArr = [];
-  //iteration
+  //make it run
   arr.forEach((item, i) => {
-    if (item % 3 === 0) {
+    if (item % 2 != 0) {
       newArr.push(item);
     }
     console.log(newArr);
   });
-
 }
 
-newArrOdd([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]);
+newArrOdd([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
